b38a24da0ff94cbcf0e3a07e4a61f29027b3490f
Michael Craun <michaelcraun@michaels-macbook-air.local>
2017-12-01 06:31:53 -0500

    Section 2, Lesson 11

--- htchhkr-dev/AppDelegate.swift
+++ htchhkr-dev/AppDelegate.swift
@@ -7,15 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    fileprivate var containerVC = ContainerVC()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        containerVC = ContainerVC()
+        
+        window?.rootViewController = containerVC
+        window?.makeKeyAndVisible()
+        
+        FIRApp.configure()
+        
         return true
     }
 
